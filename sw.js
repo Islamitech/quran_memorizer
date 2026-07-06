b6a21954262aa44e35fe2b4d69d1bbd2c7ac180b
Kareem Ezzaldin <kareem.ezz.1972@gmail.com>
2026-07-06 19:23:28 +0300

    fix: inject self-healing script to unregister older service workers and clear caches on client update; bump cache to v91

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker المتقدم
 
-const CACHE_VERSION = 'v90';
+const CACHE_VERSION = 'v91';
 const CACHE_NAMES = {
   static: `static-${CACHE_VERSION}`,
   audio: `audio-${CACHE_VERSION}`,
