819a9949f5e750229b979fc9a4b91dd9c2c87261
Kareem Ezzaldin <kareem.ezz.1972@gmail.com>
2026-07-16 05:00:37 +0300

    إظهار اسم التطبيق محفظ القران الكريم على شاشات الهواتف الجوالة

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker المتقدم
 
-const CACHE_VERSION = 'v106';
+const CACHE_VERSION = 'v107';
 const CACHE_NAMES = {
   static: `static-${CACHE_VERSION}`,
   audio: `audio-${CACHE_VERSION}`,
